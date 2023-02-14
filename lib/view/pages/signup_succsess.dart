e2250a14b22596854e56b7aa62a3e12f463a25ae
FIXER3600 <guilhermecomercial51@gmail.com>
2023-02-13 21:33:52 -0300

    Criando pastas e arquivos de telas

---
+++ lib/view/pages/signup_succsess.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignupSuccsess extends StatelessWidget {
+
+  const SignupSuccsess({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
