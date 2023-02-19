48a9bd537ea3820839031ce0595ff636b63dab5a
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2023-02-19 14:10:58 -0300

    layout_resp

--- lib/view/pages/task_list.dart
+++ lib/view/pages/task_list.dart
@@ -19,40 +19,55 @@ class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
     setFullScreen();
-    // ignore: prefer_const_constructors
+
     return Scaffold(
       backgroundColor: const Color.fromRGBO(169, 1, 247, 1),
-      body: SafeArea(
-        child: Row(
-          children: [
-            Container(
-              padding: EdgeInsets.all(3),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  scale: 9,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(3),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    scale: 9,
+                  ),
+                ),
+                width: 80,
+                height: 80,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(80),
+                  ),
                 ),
               ),
-              width: 80,
-              height: 80,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(80),
-                ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Suas Listagens',
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.search),
+                  enabledBorder: UnderlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  filled: true,
+                  fillColor: Colors.white),
             ),
-            Padding(padding: EdgeInsets.only(right: 50)),
-            Text(
-              'Suas Listagens',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-          ],
-        ),
-        
+          ),
+        ],
       ),
-      
     );
   }
 }
