a2f50e31600665df1de0360a93e59750fd3a0430
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2023-02-21 00:13:58 -0300

    layout_responsivo_mquery

--- lib/view/pages/task_list.dart
+++ lib/view/pages/task_list.dart
@@ -5,7 +5,8 @@ import 'package:flutter/services.dart';
 List<String> listaTarefas = [
   '''limpar a casa Duis voluptate laboris veniam laboris Lorem esse excepteur quis id aute labore quis. Labore irure qui labore quis et duis proident amet officia. Cupidatat eu aliqua veniam ea mollit qui elit enim occaecat.''',
   '''pintar a casa Consectetur pariatur labore in nulla nulla Lorem cillum aliqua irure eu tempor commodo deserunt veniam. Eu occaecat  Do consectetur Lorem laborum tempor est sit aliqua duis mollit magna nostrud. Eu non exercitation esse et exercitation magna.''',
-  '''arrumar a casa Sunt culpa dolore dolore proident aliqua. Deserunt .'''
+  '''arrumar a casa Sunt culpa dolore dolore proident aliqua. Deserunt .''',
+  'TESTE'
 ];
 
 class TaskList extends StatefulWidget {
@@ -32,6 +33,7 @@ class _TaskListState extends State<TaskList> {
       backgroundColor: const Color.fromRGBO(169, 1, 247, 1),
       body: Column(
         children: [
+          // LOGO
           Row(
             children: [
               Container(
@@ -66,7 +68,8 @@ class _TaskListState extends State<TaskList> {
           ),
           // SEACH BOX
           Container(
-            padding: EdgeInsets.all(20),
+            width: MediaQuery.of(context).size.width * 0.94,
+            padding: EdgeInsets.only(top: 30, bottom: 15),
             child: TextField(
               decoration: InputDecoration(
                   labelText: 'Busque palavras-chave',
@@ -79,20 +82,25 @@ class _TaskListState extends State<TaskList> {
           ),
 
           // caixa de texto
-
-          box(Color.fromRGBO(255, 242, 204, 1), listaTarefas[0]),
-          box(Color.fromRGBO(199, 255, 203, 1), listaTarefas[1]),
-          box(Color.fromRGBO(232, 197, 255, 1), listaTarefas[1]),
-
-          const SizedBox(
-            height: 5,
-            width: 5,
+          Container(
+            child: Column(
+              children: [
+                box(Color.fromRGBO(255, 242, 204, 1), listaTarefas[0]),
+                box(Color.fromRGBO(199, 255, 203, 1), listaTarefas[1]),
+                box(Color.fromRGBO(232, 197, 255, 1), listaTarefas[2]),
+              ],
+            ),
           ),
 
-          const Icon(
-            Icons.add,
-            size: 75,
-            color: Colors.white,
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed('\task_register');
+            },
+            child: const Icon(
+              Icons.add,
+              size: 75,
+              color: Colors.white,
+            ),
           )
         ],
       ),
@@ -103,8 +111,8 @@ class _TaskListState extends State<TaskList> {
     return Column(
       children: [
         Container(
-          padding: EdgeInsets.all(8),
-          width: 320,
+          padding: EdgeInsets.all(8.0),
+          width: MediaQuery.of(context).size.width * 0.94,
           height: 100,
           decoration: BoxDecoration(
             color: cor,
@@ -116,7 +124,7 @@ class _TaskListState extends State<TaskList> {
         ),
         const SizedBox(
           height: 15,
-          width: 15,
+          //width: 15,
         ),
       ],
     );
